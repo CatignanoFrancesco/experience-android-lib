4506ad4cad29e96bd1c892363dcd2216e58205c6
CatignanoFrancesco <catignanof@gmail.com>
2023-02-24 13:08:06 +0100

    Ridotti i parametri in AddExperienceAlertDialog.show

--- experience-lib/src/main/java/it/uniba/eculturetool/experience_lib/AddExperienceAlertDialog.java
+++ experience-lib/src/main/java/it/uniba/eculturetool/experience_lib/AddExperienceAlertDialog.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import androidx.appcompat.app.AlertDialog;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 import it.uniba.eculturetool.tag_lib.tag.model.LanguageTag;
 
@@ -12,7 +13,7 @@ public class AddExperienceAlertDialog {
 
     private AddExperienceAlertDialog() {}
 
-    public static void show(Context context, String operaId, List<LanguageTag> languageTags, Runnable onOkClick, Runnable onImportClick) {
+    public static void show(Context context, Consumer<Integer> onOkClick, Runnable onImportClick) {
         String puzzle = context.getString(R.string.puzzle);
         String quiz = context.getString(R.string.quiz);
         String findTheDifference = context.getString(R.string.find_the_difference);
@@ -24,7 +25,7 @@ public class AddExperienceAlertDialog {
                 .setTitle(context.getString(R.string.add_experience_title))
                 .setItems(
                         new String[]{puzzle, quiz, findTheDifference, pattern, findRfid, findDetails},
-                        (dialogInterface, i) -> onOkClick.run()
+                        (dialogInterface, i) -> onOkClick.accept(i)
                 )
                 .setNeutralButton(context.getString(R.string.import_experience), ((dialogInterface, i) -> onImportClick.run()))
                 .show();
