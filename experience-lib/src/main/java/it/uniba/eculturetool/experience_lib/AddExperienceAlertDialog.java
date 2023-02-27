cfe706af7b586a4113bb06b239435a58501c62b2
CatignanoFrancesco <catignanof@gmail.com>
2023-02-27 21:34:12 +0100

    Aggiunto un vincolo sull'aggiunta di experience in AddExperienceAlertDialog

--- experience-lib/src/main/java/it/uniba/eculturetool/experience_lib/AddExperienceAlertDialog.java
+++ experience-lib/src/main/java/it/uniba/eculturetool/experience_lib/AddExperienceAlertDialog.java
@@ -7,13 +7,20 @@ import androidx.appcompat.app.AlertDialog;
 import java.util.List;
 import java.util.function.Consumer;
 
+import it.uniba.eculturetool.experience_lib.models.Experience;
+import it.uniba.eculturetool.experience_lib.models.FindDetails;
+import it.uniba.eculturetool.experience_lib.models.FindRFID;
+import it.uniba.eculturetool.experience_lib.models.FindTheDifference;
+import it.uniba.eculturetool.experience_lib.models.Pattern;
+import it.uniba.eculturetool.experience_lib.models.Puzzle;
+import it.uniba.eculturetool.experience_lib.models.Quiz;
 import it.uniba.eculturetool.tag_lib.tag.model.LanguageTag;
 
 public class AddExperienceAlertDialog {
 
     private AddExperienceAlertDialog() {}
 
-    public static void show(Context context, Consumer<Integer> onOkClick, Runnable onImportClick) {
+    public static void show(Context context, List<Experience> addedExperiences, Consumer<Experience> onOkClick, Runnable onImportClick) {
         String puzzle = context.getString(R.string.puzzle);
         String quiz = context.getString(R.string.quiz);
         String findTheDifference = context.getString(R.string.find_the_difference);
@@ -25,7 +32,44 @@ public class AddExperienceAlertDialog {
                 .setTitle(context.getString(R.string.add_experience_title))
                 .setItems(
                         new String[]{puzzle, quiz, findTheDifference, pattern, findRfid, findDetails},
-                        (dialogInterface, i) -> onOkClick.accept(i)
+                        (dialogInterface, i) -> {
+                            Experience experience;
+                            switch (i) {
+                                case 0:
+                                    experience = new Puzzle();
+                                    break;
+                                case 1:
+                                    experience = new Quiz();
+                                    break;
+                                case 2:
+                                    experience = new FindTheDifference();
+                                    break;
+                                case 3:
+                                    experience = new Pattern();
+                                    break;
+                                case 4:
+                                    experience = new FindRFID();
+                                    break;
+                                case 5:
+                                    experience = new FindDetails();
+                                    break;
+                                default: experience = null;
+                            }
+
+                            if(addedExperiences != null) {
+                                for(Experience exp : addedExperiences) {
+                                    if(experience.getClass().equals(exp.getClass())) {
+                                        new AlertDialog.Builder(context)
+                                                .setTitle(R.string.warning)
+                                                .setMessage(R.string.error_selection)
+                                                .setNeutralButton(R.string.understand, (dialogInterface1, i1) -> {})
+                                                .show();
+                                        return;
+                                    }
+                                }
+                            }
+                            onOkClick.accept(experience);
+                        }
                 )
                 .setNeutralButton(context.getString(R.string.import_experience), ((dialogInterface, i) -> onImportClick.run()))
                 .show();
