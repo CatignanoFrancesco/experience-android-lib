92e5dd63546d78efd62304e20479cd0f4b4a0df9
CatignanoFrancesco <catignanof@gmail.com>
2023-03-17 13:53:00 +0100

    Creata l'interfaccia ExperienceStorage

---
+++ experience-lib/src/main/java/it/uniba/eculturetool/experience_lib/saving/ExperienceStorage.java
@@ -0,0 +1,23 @@
+package it.uniba.eculturetool.experience_lib.saving;
+
+import android.graphics.Bitmap;
+
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+import it.uniba.eculturetool.experience_lib.models.Answer;
+import it.uniba.eculturetool.experience_lib.models.Experience;
+import it.uniba.eculturetool.experience_lib.models.Question;
+
+public interface ExperienceStorage {
+    void saveExperience(Object operaId, Experience experience);
+    Set<Experience> getExperiencesByOperaId(Object operaId);
+    void getQuestionImageById(Object questionId, Consumer<Bitmap> onImageReady, Consumer<String> onFailureListener);
+    void getPuzzleImageById(Object puzzleId, Consumer<Bitmap> onImageReady, Consumer<String> onFailureListener);
+    void getFindTheDifferenceImagesById(Object ftdId, BiConsumer<Bitmap, Bitmap> onImagesReady, Consumer<String> onFailureListener);
+    void getFindDetailsImageById(Object findDetailsId, Consumer<Bitmap> onImageReady, Consumer<String> onFailureListener);
+    void removeExperienceById(Object operaId, Experience experience);
+    void removeQuestionById(String quizId, Question question);
+    void removeAnswerById(String quizId, String questionId, Answer answer);
+}
