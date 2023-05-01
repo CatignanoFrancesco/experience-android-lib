8c3a26b018d35a6faa0d08f8b342e8b888bd39ce
f.catignano <f.catignano@studenti.uniba.it>
2023-05-01 13:57:07 +0200

    Creato SingleQuestionFragment

---
+++ experience-lib/src/main/java/it/uniba/eculturetool/experience_lib/fragments/singlequestion/SingleQuestionFragment.java
@@ -0,0 +1,70 @@
+package it.uniba.eculturetool.experience_lib.fragments.singlequestion;
+
+import static it.uniba.eculturetool.experience_lib.ExperienceEditorFragment.KEY_EXPERIENCE_ID;
+import static it.uniba.eculturetool.experience_lib.ExperienceEditorFragment.KEY_OPERA_ID;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import it.uniba.eculturetool.experience_lib.ExperienceDataHolder;
+import it.uniba.eculturetool.experience_lib.fragments.quiz.QuizEditorFragment;
+import it.uniba.eculturetool.experience_lib.ui.SingleQuestionUI;
+
+public class SingleQuestionFragment extends Fragment {
+    private final SingleQuestionUI ui = SingleQuestionUI.getInstance();
+
+    private String operaId;
+    private String singleQuestionId;
+    private final ExperienceDataHolder experienceDataHolder = ExperienceDataHolder.getInstance();
+
+    private EditText questionEditText;
+    private RecyclerView answersRecyclerView;
+    private Button saveButton, addAnswerButton;
+
+    public SingleQuestionFragment() {}
+
+    public static SingleQuestionFragment newInstance(String operaId, String singleQuestionId) {
+        SingleQuestionFragment fragment = new SingleQuestionFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString(KEY_OPERA_ID, operaId);
+        bundle.putString(KEY_EXPERIENCE_ID, singleQuestionId);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            operaId = getArguments().getString(KEY_OPERA_ID);
+            singleQuestionId = getArguments().getString(KEY_EXPERIENCE_ID);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(ui.singleQuestionEditorUi.layout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        questionEditText = view.findViewById(ui.singleQuestionEditorUi.questionEditText);
+        answersRecyclerView = view.findViewById(ui.singleQuestionEditorUi.answersRecyclerViewId);
+        addAnswerButton = view.findViewById(ui.singleQuestionEditorUi.addAnswerButtonId);
+        saveButton = view.findViewById(ui.singleQuestionEditorUi.saveButtonId);
+    }
+}
