f02de892e9547c907c8a5526e478483d8844306c
f.catignano <f.catignano@studenti.uniba.it>
2023-05-03 11:35:17 +0200

    Creato HitTheEnemyListFragment

---
+++ experience-lib/src/main/java/it/uniba/eculturetool/experience_lib/fragments/hittheenemy/HitTheEnemyListFragment.java
@@ -0,0 +1,43 @@
+package it.uniba.eculturetool.experience_lib.fragments.hittheenemy;
+
+import static it.uniba.eculturetool.experience_lib.ExperienceEditorFragment.KEY_EXPERIENCE_ID;
+import static it.uniba.eculturetool.experience_lib.ExperienceEditorFragment.KEY_OPERA_ID;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import it.uniba.eculturetool.experience_lib.R;
+public class HitTheEnemyListFragment extends Fragment {
+    private String operaId;
+    private String hitTheEnemyId;
+
+    public HitTheEnemyListFragment() {}
+
+    public static HitTheEnemyListFragment newInstance(String operaId, String hitTheEnemyId) {
+        HitTheEnemyListFragment fragment = new HitTheEnemyListFragment();
+        Bundle args = new Bundle();
+        args.putString(KEY_OPERA_ID, operaId);
+        args.putString(KEY_EXPERIENCE_ID, hitTheEnemyId);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            operaId = getArguments().getString(KEY_OPERA_ID);
+            hitTheEnemyId = getArguments().getString(KEY_EXPERIENCE_ID);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_hit_the_enemy_list, container, false);
+    }
+}
