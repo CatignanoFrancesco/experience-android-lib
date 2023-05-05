cbf7ed2f69f4b67c02a412d268e9ad8c4b6a63c3
f.catignano <f.catignano@studenti.uniba.it>
2023-05-05 13:54:11 +0200

    Creato il dialog per scegliere il questionario

--- experience-lib/src/main/java/it/uniba/eculturetool/experience_lib/fragments/questionnaire/QuestionnaireFragment.java
+++ experience-lib/src/main/java/it/uniba/eculturetool/experience_lib/fragments/questionnaire/QuestionnaireFragment.java
@@ -3,14 +3,20 @@ package it.uniba.eculturetool.experience_lib.fragments.questionnaire;
 import static it.uniba.eculturetool.experience_lib.ExperienceEditorFragment.KEY_EXPERIENCE_ID;
 import static it.uniba.eculturetool.experience_lib.ExperienceEditorFragment.KEY_OPERA_ID;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toolbar;
 
 import it.uniba.eculturetool.experience_lib.ExperienceDataHolder;
 import it.uniba.eculturetool.experience_lib.R;
@@ -24,6 +30,10 @@ public class QuestionnaireFragment extends Fragment {
 
     private String operaId;
     private String questionnaireId;
+
+    private Toolbar toolbar;
+    private RecyclerView recyclerView;
+    private Button saveButton;
 
     public QuestionnaireFragment() {}
 
@@ -51,5 +61,24 @@ public class QuestionnaireFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(ui.questionnaireFragmentUi.layout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        toolbar = view.findViewById(ui.questionnaireFragmentUi.toolbar);
+        recyclerView = view.findViewById(ui.questionnaireFragmentUi.questionsRecyclerView);
+        saveButton = view.findViewById(ui.questionnaireFragmentUi.saveButton);
+    }
+
+    private void chooseQuestionnaireDialog() {
+        new AlertDialog.Builder(requireContext())
+                .setTitle(R.string.choose_questionnaire)
+                .setCancelable(false)
+                .setNegativeButton(R.string.back, (dialogInterface, i) -> {
+                    dialogInterface.dismiss();
+                    requireActivity().finish();
+                }).show();
     }
 }
